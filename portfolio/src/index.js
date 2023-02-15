d99559c29e1377b548ae5268b3d6174183535862
Sheru <100682913+SheruShafiq@users.noreply.github.com>
2023-02-15 10:46:17 +0100

    Three JS Journey Practice

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Canvas  } from '@react-three/fiber';
+import './style.css'
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   
+  <Canvas>
+    <App />
+  </Canvas>
 
   </>
 );
