4547acbb876ef8eafcf31b114403d5800a111ba3
Sheru <100682913+SheruShafiq@users.noreply.github.com>
2023-02-16 10:42:12 +0100

    quick commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,40 +1,34 @@
 import React from "react";
 import "./App.css";
-import { useThree, extend, useFrame } from '@react-three/fiber'
+import { OrbitControls, TransformControls, PivotControls } from "@react-three/drei";
 import { useRef } from "react";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import Object from "./Object.js";
-extend({ OrbitControls })
 
 
 function App() {
 
-  const cubeRef = useRef();
-  const { camera, gl } = useThree()
-
-  useFrame(({ clock }) => {
-   cubeRef.current.rotation.y = clock.getElapsedTime();
-  });
+const refC = useRef()
 
   return (
     <>
-     <orbitControls args={ [ camera, gl.domElement ] } />
+     <OrbitControls makeDefault/>
   <directionalLight position={[0, 1, 0]} intensity={2} />   
 <ambientLight intensity={0.2} />
 
 
       {/* The purple box on the right */}
-      <mesh ref={cubeRef} scale={1.5} position-x={2} rotation-y={Math.PI * 0.20}>
+      <mesh scale={1.5} ref={refC} position-x={2} rotation-y={Math.PI * 0.20}>
         <boxGeometry scale={1.5} />
-        <meshStandardMaterial args={[{ color: "purple" }]} />
+        <meshStandardMaterial args={[{ color: "red" }]} />
       </mesh>
+      <TransformControls object={refC} />
+
       {/* The orange wireframe sphere on the left */}
-      <mesh scale={1} position-x={-2}>
+      <mesh scale={1}  position-x={-2} >
         <sphereGeometry args={[1, 50, 50]} />
         <meshStandardMaterial args={[{ color: "orange", wireframe: false }]} />
       </mesh>
+      
 
-<Object />
 
       {/* The plane underneath them */}
       <mesh rotation-x={- Math.PI * 0.4} scale={[10, 5, 2]} position-y={-1}>
